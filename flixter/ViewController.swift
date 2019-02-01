19bc40525c3c2f1955db6396e95669a1b6efdf6e
Mayra Ochoa <ocho1356@m506-118-26462.local>
2019-02-01 14:06:05 -0800

    Initial Commit

---
+++ flixter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flixter
+//
+//  Created by Mayra Ochoa on 2/1/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
